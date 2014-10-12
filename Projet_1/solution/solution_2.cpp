c57695e25a36a9c79d4fb948d1a920b36385e0cb
Nathanaël COURET <couretn@beloit.edu>
2014-10-12 16:39:00 +0200

    Solution 2 qui marche :)

--- Projet_1/solution/solution_2.cpp
+++ Projet_1/solution/solution_2.cpp
@@ -5,13 +5,14 @@ using namespace std;
 #include "common.h"
 
 /**
+* Compte le nombre de cases sur une ligne donnée, depuis une position donnée.
+* s'arrête en fin de ligne ou dés que la dalle est noire.
+*
+* dalle: la dallage contenant la ligne à traiter.
+* x: l'abscisse de départ du comptage.
+* y: l'ordonnée de départ du comptage.
 */
 int measureLine(const dalle& dalle, int x, int y) {
-	if(x >= dalle.dim.width) {
-		throw new range_error("Index de départ supérieur à la largeur du dallage");
-	} else if(y >= dalle.dim.height) {
-		throw new range_error("Index de ligne supérieur à la hauteur du dallage");
-	}
 	int width = 0;
 	// on parcourt la ligne en cours depuis l'index start.x jusqu'à la fin de ligne ou une dalle noire
 	for(int i = x; dalle.data[dalle.dim.width*y+i] && i < dalle.dim.width; i++ ) {
@@ -21,28 +22,41 @@ int measureLine(const dalle& dalle, int x, int y) {
 	return width;
 }
 
+/**
+* Recherche le plus grand rectangle de dalles blanches depuis la postion donnée.
+* Enumère tous les rectangles possibles avec pour origine une case donnée et sélectionne le plus grand.
+*
+* dalle: le dallage dans lequel on recherche le rectangle.
+* max: le plus grand rectangle.
+* start: les coordonnées du coin supérieur gauche des rectangles à énumérer.
+* coord: les coordonnées du coin supérieur gauche du plus grand rectangle.
+*/
 void rectTraverse(const dalle& dalle, rect& max, const point& start, point& coord) {
-	rect current = { .width=measureLine(dalle, start.x, start.y), .height=1};
+	rect current = { .width=dalle.dim.width, .height=0};
+
 	for(int j = start.y; j < dalle.dim.height; j++) {
+		//on évalue chaque ligne jusqu'à ce que la premièe case soit noire.
 		if(!dalle.data[j*dalle.dim.width+start.x]) { break;}
 
 		int ligne = measureLine(dalle, start.x, j);
 		current.width = (ligne < current.width) ? ligne : current.width;
 		current.height++;
+		//si le rectangle calculé est plus grand que le plus grand actuel on le remplace.
 		if(current > max) {
 			max = current;
 			coord = start;
 		}
 	}
-	//temporaire, pour les rect de 1 case.
-	if(current > max) {
-		max = current;
-		coord = start;
-	}
 }
 
-void searchRect(const dalle& dalle, rect& r, point& coord) {
-	rect max = r;
+/**
+* Enumère tout les coins supérieurs gauche possibles pour un rectangle blanc puis recherche le plus grand rectangle. 
+* 
+* dalle: le dallage dans lequel on recherche le rectangle.
+* max: le plus grand rectangle.
+* coord: les coordonnées du coin supérieur gauche du plus grand rectangle.
+*/
+void searchRect(const dalle& dalle, rect& max, point& coord) {
 	for(int i = 0; i < dalle.dim.width; i++) {
 		for(int j = 0; j < dalle.dim.height; j++) {
 			if(dalle.data[j*dalle.dim.width+i]) {
@@ -53,8 +67,12 @@ void searchRect(const dalle& dalle, rect& r, point& coord) {
 	}
 }
 
+//////////////////////////////////////////////////////////////////////////////////////////
+//                            Point d'entrée du programme                               //
+//////////////////////////////////////////////////////////////////////////////////////////
 int main(int argc, char* argv[]) {
 
+	//on vérifie qu'un fichier est donné en entrée.
 	if(argc != 2) {
 		cout << "ERRREUR sur les paramètres d'appel" << endl;
 		return -1;
@@ -73,6 +91,7 @@ int main(int argc, char* argv[]) {
 
 	bool black = false;
 	bool white = false;
+	//on vérifie la dalle et on l'affiche.
 	checkDalle(dalle, white, black, true);
 
 	if(black && !white) {
@@ -82,17 +101,24 @@ int main(int argc, char* argv[]) {
 		return 0;
 	}
 	rect rect = {.width=0, .height=0};
-	point coords = {.x=0, .y=0};
+	point coord = {.x=0, .y=0};
 
 	if(!black && white) {
 		// si la dalle est toute blanche
 		rect.width = dalle.dim.width;
 		rect.height = dalle.dim.height;
 	} else {
-		searchRect(dalle, rect, coords);
+		//sinon lance la recherche.
+		searchRect(dalle, rect, coord);
 	}
 
-	cout << "rectangle max: " << rect.width << "," << rect.height << " a (" << coords.x << "," << coords.y << ")" << endl;
+	//Affichage
+	cout << endl;
+	cout << "Caractéristique du plus grand rectangle :" << endl;
+	cout << "- coordonnée x: " << coord.x << endl;
+	cout << "- coordonnée y: " << coord.y << endl;
+	cout << "- largeur : "     << rect.width << endl;
+	cout << "- hauteur : "     << rect.height << endl;
 
 	delete[] dalle.data;
 	return 0;
